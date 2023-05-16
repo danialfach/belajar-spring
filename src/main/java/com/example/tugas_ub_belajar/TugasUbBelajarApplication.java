e06c0c154876dcd2f347bf661f997fbc5788f36a
danialfach <danialfacrudin@gmail.com>
2023-05-16 12:47:24 +0700

    belajar spring

---
+++ src/main/java/com/example/tugas_ub_belajar/TugasUbBelajarApplication.java
@@ -0,0 +1,13 @@
+package com.example.tugas_ub_belajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasUbBelajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasUbBelajarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tugas_ub_belajar/TugasUbBelajarApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.tugas_ub_belajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasUbBelajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// E1 = Danial Fachrudin
+	}
+
+}
